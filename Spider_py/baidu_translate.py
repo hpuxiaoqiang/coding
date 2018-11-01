949eae2d43cd2824e0de7ed0e788623e3d3395a0
hpuxiaoqiang <hpuxiaoqiang@126.com>
2018-11-01 18:32:22 +0800

    文件更改

--- Spider_py/baidu_translate.py
+++ Spider_py/baidu_translate.py
@@ -4,6 +4,11 @@ import hashlib
 import random
 import urllib.parse
 import json
+#将翻译功能封装
+#words必传参数,为需要翻译的文字
+#fromlang为待翻译文字语言,默认为zh
+#tolang为翻译后文字语言,默认为en
+#对实例对象调用trans()方法就得到了翻译后的文字
 class Translate(object):
     def __init__(self,words,fromlang='zh',tolang='en'):
         self.fromlang = fromlang
@@ -15,8 +20,11 @@ class Translate(object):
         self.__salt = random.randint(32768, 65536)
         self.__md5key = hashlib.md5()
     def __get_sign(self):
+        #得到我的sign串
         self.__sign = self.__appid+self.words+str(self.__salt)+self.__secretkey
+        #计算我的sign串的md5值
         self.__md5key.update(self.__sign.encode('utf-8'))
+        #构造翻译当前文字的URL
         myurl = self.__url + '?q=' + \
                 urllib.parse.quote(self.words) \
                 + '&from=' + self.fromlang \
@@ -29,6 +37,7 @@ class Translate(object):
         res = requests.get(self.__get_sign())
         json_data = json.loads(res.text)
         dst = json_data.get("trans_result")[0].get("dst")
+        #返回翻译结果
         return dst
 
 # fromlang = 'zh'
@@ -49,6 +58,3 @@ class Translate(object):
 # json_data = json.loads(res.text)
 # print(json_data.get("trans_result")[0].get("dst"))
 # # print(json_data.get("trans_result").get("dst"))
-
-tr = Translate('I want to go to Beijing',fromlang='auto',tolang='zh')
-print(tr.trans())
